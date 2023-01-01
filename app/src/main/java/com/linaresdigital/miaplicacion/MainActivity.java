ac34f8c233e6cbdb5da054d66b0a6c082c0e37b8
Óscar García <ojgarciab@users.noreply.github.com>
2023-01-01 18:05:58 +0100

    Cambio para obtener un InputStream con el archivo

--- app/src/main/java/com/linaresdigital/miaplicacion/MainActivity.java
+++ app/src/main/java/com/linaresdigital/miaplicacion/MainActivity.java
@@ -2,11 +2,15 @@ package com.linaresdigital.miaplicacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentProviderClient;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,15 +22,21 @@ public class MainActivity extends AppCompatActivity {
         TextView textView1 = (TextView)findViewById(R.id.texto1);
         TextView textView2 = (TextView)findViewById(R.id.texto2);
         try {
-            String ruta = getIntent().getData().getPath();
-            ruta = ruta.substring(ruta.indexOf(":") + 1);
-            textView1.setText(ruta);
-            File file = new File(ruta);
-            textView2.setText(file.exists() ? "Existe" : "No existe");
+            Uri uri = getIntent().getData();
+            textView1.setText(getIntent().getData().getPath());
+            InputStream file = getContentResolver().openInputStream(uri);
+            /* Leemos el primer octeto del archivo:
+                -1: hubo un error leyendo el archivo (no se abrió, está vacío, etc)
+                75: código ASCII del primer carácter de un archivo EPUB
+                    (los dos primeros son "PK" al ser un ZIP)
+            */
+            textView2.setText(
+                    ((file.read() != -1) ? "Abierto" : "No abierto")
+                    + ": " + file.read()
+            );
         } catch (Exception e) {
             textView1.setText("Apertura normal");
             textView2.setText("de la aplicación");
         }
-
     }
 }
